8b4090979fc8aac1266046878f21ee20ddd3ffbe
Cliff Whitworth <cliff.whitworth@gmail.com>
2018-08-27 16:54:43 -0500

    prod setup

---
+++ config/keys.js
@@ -0,0 +1,9 @@
+// keys.js - credentials to commit
+
+if(process.env.NODE_ENV === 'production') {
+  // return production keys
+  module.exports = require('./prod');
+} else {
+  // return dev keys
+  module.exports = require('./dev');
+}
